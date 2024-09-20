503bacf7e69cc7e4b8b0a16c2a7634833fe77fea
Sivaselvan <mvs@buffalo.edu>
2024-09-20 12:57:22 -0400

    Created Elements library

---
+++ src/Elements/Cable_mod.f90
@@ -0,0 +1,18 @@
+module Cable
+
+use emxArray
+
+implicit none
+
+contains
+
+!--------------------------------------------------------
+
+subroutine cable_setup
+
+
+end subroutine cable_setup
+    
+!--------------------------------------------------------
+    
+end module Cable
